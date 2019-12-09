6d818621e036dbafda5ef4546c6126301b0d8a88
Remo Kessler <remo.kessler@gmx.ch>
2019-12-09 16:57:39 +0100

    Programming lesson 5

---
+++ Lession05/Lession05/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Lession05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblGoal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pnlSlider = new System.Windows.Forms.Panel();
+            this.pnlProjectile = new System.Windows.Forms.Panel();
+            this.TmrProjectile = new System.Windows.Forms.Timer(this.components);
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(199, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health:";
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.ForeColor = System.Drawing.Color.White;
+            this.lblHealth.Location = new System.Drawing.Point(258, 37);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(16, 17);
+            this.lblHealth.TabIndex = 1;
+            this.lblHealth.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(332, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Points:";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.ForeColor = System.Drawing.Color.White;
+            this.lblPoints.Location = new System.Drawing.Point(385, 37);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(16, 17);
+            this.lblPoints.TabIndex = 3;
+            this.lblPoints.Text = "0";
+            // 
+            // lblGoal
+            // 
+            this.lblGoal.AutoSize = true;
+            this.lblGoal.ForeColor = System.Drawing.Color.White;
+            this.lblGoal.Location = new System.Drawing.Point(511, 37);
+            this.lblGoal.Name = "lblGoal";
+            this.lblGoal.Size = new System.Drawing.Size(24, 17);
+            this.lblGoal.TabIndex = 5;
+            this.lblGoal.Text = "25";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(458, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Goal:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(544, 75);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(56, 500);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pnlSlider
+            // 
+            this.pnlSlider.BackColor = System.Drawing.Color.Aqua;
+            this.pnlSlider.Location = new System.Drawing.Point(520, 450);
+            this.pnlSlider.Name = "pnlSlider";
+            this.pnlSlider.Size = new System.Drawing.Size(15, 100);
+            this.pnlSlider.TabIndex = 7;
+            // 
+            // pnlProjectile
+            // 
+            this.pnlProjectile.BackColor = System.Drawing.Color.Aqua;
+            this.pnlProjectile.Location = new System.Drawing.Point(163, 236);
+            this.pnlProjectile.Name = "pnlProjectile";
+            this.pnlProjectile.Size = new System.Drawing.Size(25, 25);
+            this.pnlProjectile.TabIndex = 8;
+            // 
+            // TmrProjectile
+            // 
+            this.TmrProjectile.Interval = 1;
+            this.TmrProjectile.Tick += new System.EventHandler(this.TmrProjectile_Tick);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.btnStartStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartStop.ForeColor = System.Drawing.Color.White;
+            this.btnStartStop.Location = new System.Drawing.Point(12, 12);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(176, 42);
+            this.btnStartStop.TabIndex = 9;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(585, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(612, 583);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.pnlProjectile);
+            this.Controls.Add(this.pnlSlider);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lblGoal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label lblGoal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel pnlSlider;
+        private System.Windows.Forms.Panel pnlProjectile;
+        private System.Windows.Forms.Timer TmrProjectile;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lession05/Lession05/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lession05
+{
+    public partial class Form1 : Form
+    {
+        #region declaration
+        private Random rand = new Random();
+        private int speed = 1;
+        private int points = 0;
+        private int Points
+        {
+            get => points;
+            set
+            {
+                lblPoints.Text = value.ToString();
+                points = value;
+            }
+        }
+        private int goal = 25;
+        private int health = 3;
+        private int Health
+        {
+            get => health;
+            set
+            {
+                lblHealth.Text = value.ToString();
+                health = value;
+            }
+        }
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pnlSlider.Location = new Point(pnlSlider.Location.X, (int)((50 - trackBar1.Value) * 7.5)+35);
+        }
+        private void TmrProjectile_Tick(object sender, EventArgs e)
+        {
+            if(pnlProjectile.Bounds.IntersectsWith(pnlSlider.Bounds))
+            {
+                Points+=1;
+                if (Points == goal) StopGame("You've won congratulations!");
+                ResetProjectile();
+            } 
+            if(pnlProjectile.Bounds.IntersectsWith(trackBar1.Bounds))
+            {
+                Health-=1;
+                if (Health == 0) StopGame("You've lost, noob!");
+                ResetProjectile();
+            }
+            pnlProjectile.Location = new Point(pnlProjectile.Location.X+speed, pnlProjectile.Location.Y);
+        }
+        private void ResetProjectile()
+        {
+            pnlProjectile.Location = new Point(10, rand.Next(75, 400));
+            speed = rand.Next(2, 8);
+        }
+        private void StopGame(string s = "")
+        {
+            TmrProjectile.Enabled = false;
+            MessageBox.Show(s);
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (TmrProjectile.Enabled)
+                btnStartStop.Text = "Stop";
+            else
+                btnStartStop.Text = "Start";
+            TmrProjectile.Enabled = !TmrProjectile.Enabled;
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.Aqua;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.White;
+        }
+    }
+}
