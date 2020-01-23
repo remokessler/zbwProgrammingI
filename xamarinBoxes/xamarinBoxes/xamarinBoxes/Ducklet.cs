ac2658a23427011d137648e0f2143a455e25baf8
Remo Kessler <remo.kessler@gmx.ch>
2020-01-23 19:15:40 +0100

    Ducklets

---
+++ xamarinBoxes/xamarinBoxes/xamarinBoxes/Ducklet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinBoxes
+{
+    public class Ducklet
+    {
+        public string Message;
+        public string Url;
+    }
+}
